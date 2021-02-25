80363a431df5603fb02cc53deac99151737625b6
QuentinBeauchet <quentinbeauchet@free.fr>
2021-02-25 13:29:59 +0100

    cell

--- src/main/java/Cell.java
+++ src/main/java/Cell.java
@@ -1,11 +1,26 @@
-public  enum Cell {
-    VOID,BORDER,SPACE;
+public class Cell{
+    public enum CellType {
+        BORDER, NORMAL, DOOR;
+    }
+    private final boolean isAccesible;
+    private final Position pos;
+    private final CellType type;
 
-    @Override
-    public String toString() {
-        if (VOID == this)return "/";
-        else if(BORDER == this)return "+";
-        else if (SPACE == this)return ".";
-        return " ";
+    Cell(int x, int y, boolean isAccesible, CellType type) {
+        this.pos = new Position(x,y);
+        this.isAccesible = isAccesible;
+        this.type = type;
+    }
+
+    public boolean isAccesible() {
+        return isAccesible;
+    }
+
+    public Position getPos() {
+        return pos;
+    }
+
+    public CellType getType() {
+        return type;
     }
 }
