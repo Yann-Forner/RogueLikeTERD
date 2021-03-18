9d1a84a3d9dce8ef55a03164c4c9382a2365ce71
QuentinBeauchet <quentinbeauchet@free.fr>
2021-03-18 15:22:48 +0100

    PRobelem

--- src/main/java/Model/Map/Strategy/NormalRoomStrategy.java
+++ src/main/java/Model/Map/Strategy/NormalRoomStrategy.java
@@ -1,13 +1,20 @@
 package Model.Map.Strategy;
 
 
+import Model.Affichage;
 import Model.Cell;
 import Model.Map.Room;
 
 public class NormalRoomStrategy extends RoomStrategy{
     @Override
     public void composeRoom(Room r) {
-        super.composeRoom(r);
-        r.fillMap(new Cell(true, Cell.CellType.NORMAL));
+        //super.composeRoom(r);
+        r.fillMap(new Cell(true, Cell.CellType.PATH){
+                      @Override
+                      public String toString() {
+                          return Affichage.GREEN+'.';
+                      }
+                  }
+        );
     }
 }
