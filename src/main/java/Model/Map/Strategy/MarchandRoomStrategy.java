895656e962fec47f9815ad3de0d657ee90272ba8
QuentinBeauchet <quentinbeauchet@free.fr>
2021-03-18 14:37:45 +0100

    upndown

--- src/main/java/Model/Map/Strategy/MarchandRoomStrategy.java
+++ src/main/java/Model/Map/Strategy/MarchandRoomStrategy.java
@@ -2,12 +2,18 @@ package Model.Map.Strategy;
 
 import Model.Cell;
 import Model.Map.Room;
+import Model.Position;
+import Model.Procedure;
+
+import javax.swing.*;
 
 public class MarchandRoomStrategy implements RoomStrategy{
     @Override
     public void composeRoom(Room r) {
         //TODO rajouter le marchand quand il sera implémentés
-        r.set(0,0,new Cell(true,Cell.CellType.UP));
-        r.set(4,0,new Cell(true,Cell.CellType.DOWN));
+        Position p1 = Procedure.getAccesibleRandomPosition(false, r);
+        r.get(p1).updateCell(true, Cell.CellType.UP);
+        Position p2 = Procedure.getAccesibleRandomPosition(false, r);
+        r.get(p2).updateCell(true, Cell.CellType.DOWN);
     }
 }
