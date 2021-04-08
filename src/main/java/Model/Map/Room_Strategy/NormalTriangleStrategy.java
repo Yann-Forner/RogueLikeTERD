4ebbde20ba1c60d8ce37944cbc36d08d380cacf3
QuentinBeauchet <quentinbeauchet@free.fr>
2021-04-08 14:26:23 +0200

    Palmier et barriere

--- src/main/java/Model/Map/Room_Strategy/NormalTriangleStrategy.java
+++ src/main/java/Model/Map/Room_Strategy/NormalTriangleStrategy.java
@@ -8,7 +8,7 @@ public class NormalTriangleStrategy extends TriangleRoomStrategy{
     @Override
     public void composeRoom(Room r) {
         super.fillTriangle(r);
-        System.out.println(r);
+        setStyleCell(r);
     }
 
     @Override
