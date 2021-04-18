05087d1785e36a85a48c708b03a79f5166f943f0
QuentinBeauchet <quentinbeauchet@free.fr>
2021-04-18 13:57:56 +0200

    ...

--- src/main/java/Model/Utils/TourManager.java
+++ src/main/java/Model/Utils/TourManager.java
@@ -2,7 +2,6 @@ package Model.Utils;
 
 import Model.Entitys.Monsters.AbstractMonster;
 import Model.Entitys.BasicPlayer;
-import Model.Entitys.Monsters.MonsterFactory;
 import Model.Map.Etage;
 import Model.Map.Map;
 
