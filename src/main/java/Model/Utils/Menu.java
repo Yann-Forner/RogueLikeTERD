7703c85d7cf58023aac352545d32523e418e8fae
Yann-Forner <yann.forner@hotmail.com>
2021-05-12 00:03:53 +0200

    modification du fonctionnement du menu (il reste des bugs )
    Signed-off-by: Yann-Forner <yann.forner@hotmail.com>

--- src/main/java/Model/Utils/Menu.java
+++ src/main/java/Model/Utils/Menu.java
@@ -1,9 +1,14 @@
 package Model.Utils;
-
+;
 import Model.Entitys.Items.Inventory;
 import Model.Entitys.Items.Weapons.AbstractWeapon;
 import Model.Map.Etage;
 import Model.Map.Map;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Classe servant a afficher le menu sur la droite du terminal
@@ -12,9 +17,19 @@ import Model.Map.Map;
 public class Menu {
     private Etage etage;
     private Map map;
-    private String[] menuAffichage;
-    private String[] colors;
-    private static final int LARGEUR_MENU = 81;
+    private CaseMenu[][] menuAffichage;
+    private static final int LARGEUR_MENU = 83;
+
+
+
+    private class CaseMenu{
+        public String container= " ";
+
+        @Override
+        public String toString() {
+            return container;
+        }
+    }
     /**
      * Constructeur d'un menu
      * @param etage etage
@@ -41,27 +56,27 @@ public class Menu {
                 "║"+lvl+"║ ",
                 "╚"+"═".repeat(lvl.length())+"╝ "
         };
-        printStringOnSide(0,"  SEED : "+Procedure.getSeed(),2,Affichage.GREEN);
-        printStringOnSide(1,"  TIMER : "+TourManager.getTimer()+"         ",2,Affichage.GREEN);
-        printStringOnSide(2,UpperMenu,1, Affichage.GREEN);
+        printStringOnSide(0,"  SEED : "+Procedure.getSeed(),2);
+        printStringOnSide(1,"  TIMER : "+TourManager.getTimer()+"         ",2);
+        printStringOnSide(2,UpperMenu,1);
         printLine(4,Affichage.GREEN);
-        printStringOnSide(0,"   Joueur : "+ Start.getPlayer().getNom(),6,Affichage.RED);
-        printStringOnSide(0,"   Classe : "+ Start.getPlayer().getClasse().getNom(),7,Affichage.RED);
-        printStringOnSide(0,"   Etage n°"+(map.getIndexCurrent()+1),11,Affichage.YELLOW);
-        printStringOnSide(1,niveau,6,Affichage.RED);
-        printStringOnSide(0,"   Force : "+Start.getPlayer().getForce(),8,Affichage.RED);
+        printStringOnSide(0,Affichage.RED+"   Joueur : "+ Start.getPlayer().getNom() + Affichage.BLUE+" ",6);
+        printStringOnSide(0,"   Classe : "+ Start.getPlayer().getClasse().getNom(),7);
+        printStringOnSide(0,"   Etage n°"+(map.getIndexCurrent()+1),11);
+        printStringOnSide(1,niveau,6);
+        printStringOnSide(0,"   Force : "+Start.getPlayer().getForce(),8);
         int range = 1 ;
         if(inv.getWeapons().size()!=0)range = inv.getWeapons().get(0).getRange();
 
-        printStringOnSide(0,"   Portée : "+range,9,Affichage.RED);
+        printStringOnSide(0,"   Portée : "+range,9);
 
         printLine(12,Affichage.BLUE);
 
-        printStringOnSide(0, "  Inventaire",14,Affichage.PURPLE);
+        printStringOnSide(0, "  Inventaire",14);
 
         var potionsList = inv.getPotions();
         var armesList = inv.getWeapons();
-        printStringOnSide(0, " Vous avez " + armesList.size() + " armes :",17 , Affichage.RED);
+        printStringOnSide(0, " Vous avez " + armesList.size() + " armes :",17);
 
         StringBuilder armesString = new StringBuilder();
         for (int index = 0; index < armesList.size(); index++) {
@@ -72,24 +87,24 @@ public class Menu {
                 } else {
                     armesString.append(Affichage.RED);
                 }
-                armesString.append("[" + abstractWeapon.toString() + "] ");
+                armesString.append("[" + abstractWeapon.toString() +Affichage.RESET+ "]  ");
             }
             else
-                armesString.append(abstractWeapon.toString() + " ");
+                armesString.append(abstractWeapon.toString()+Affichage.RESET + "  ");
         }
 
-        printStringOnSide(0,armesString.toString(),18,Affichage.RED);
+        printStringOnSide(0,armesString.toString(),18);
 
-       printStringOnSide(0, " Vous avez " + potionsList.size() + " potions :",20, Affichage.RED);
+       printStringOnSide(0, " Vous avez " + potionsList.size() + " potions :",20);
 
        StringBuilder potionsString = new StringBuilder();
        for (int i = 0; i < potionsList.size(); i++) {
            if (i == 0)
-               potionsString.append("[" + potionsList.get(i).toString() + "]");
+               potionsString.append("[" + potionsList.get(i).toString()+Affichage.RESET + "]  ");
            else
-               potionsString.append(potionsList.get(i).toString() + " ");
+               potionsString.append(potionsList.get(i).toString() +Affichage.RESET + "  ");
        }
-      printStringOnSide(0, potionsString.toString(),21, Affichage.RED);
+      printStringOnSide(0, potionsString.toString(),21);
     }
 
     /**
@@ -97,11 +112,25 @@ public class Menu {
      * @auhtor Yann
      */
     public void clear(){
-        menuAffichage = new String[etage.getHeigth()];
-        menuAffichage[0] = "╔═════════════════════════════════════════════════════════════════════════════════╗\n";
-        for (int i = 1; i < etage.getHeigth()-1 ; i++)menuAffichage[i]= "║                                                                                 ║\n" ;
-        menuAffichage[etage.getHeigth()-1] = "╚═════════════════════════════════════════════════════════════════════════════════╝\n";
-        colors = new String[etage.getHeigth()];
+        menuAffichage = new CaseMenu[etage.getHeigth()][LARGEUR_MENU];
+        for (int i = 0; i < menuAffichage.length ; i++) {
+            for (int j = 0; j < menuAffichage[0].length; j++) {
+                menuAffichage[i][j] = new CaseMenu();
+            }
+        }
+        printAllLine(0, "╔═════════════════════════════════════════════════════════════════════════════════╗");
+        for (int i = 1; i < etage.getHeigth()-1 ; i++)printAllLine(i, "║                                                                                 ║") ;
+        printAllLine(etage.getHeigth()-1, "╚═════════════════════════════════════════════════════════════════════════════════╝");
+    }
+
+
+    public void printAllLine(int line, String s){
+        for (int i = 0; i < menuAffichage[line].length; i++) {
+            if(i == menuAffichage[line].length-1) menuAffichage[line][i].container = Affichage.BLUE+s.charAt(i)+"\n";
+            else
+            menuAffichage[line][i].container = s.charAt(i)+"";
+
+        }
     }
 
     /**
@@ -110,7 +139,7 @@ public class Menu {
      * @param color couleur de la ligne
      */
     public void printLine (int  y,String color){
-        printStringOnLine(1,y,"═════════════════════════════════════════════════════════════════════════════════",color);
+        printStringOnLine(1,y,color+"═══════════════════════════════════════════════════════════════════════════════════════════");
 
     }
 
@@ -119,11 +148,11 @@ public class Menu {
      * @param x Coordonnée x
      * @param y Coordonnée y
      * @param myArrayString le tableau de string
-     * @param color couleur des lignes du tableau
+
      */
-    public void printOnMenu(int x, int y , String[] myArrayString, String color){
+    public void printOnMenu(int x, int y , String[] myArrayString){
         for (int i = 0; i < myArrayString.length; i++) {
-            printStringOnLine(x,y+i,myArrayString[i],color);
+            printStringOnLine(x,y+i,myArrayString[i]);
         }
     }
 
@@ -132,15 +161,32 @@ public class Menu {
      * @param x Coordonnée x
      * @param y Coordonnée y
      * @param s la string
-     * @param color couleur de la ligne du string
      */
-    public void printStringOnLine(int x, int y ,String s, String color){
-        if (s.length() > LARGEUR_MENU) s = s.substring(0, LARGEUR_MENU);
-        putColor(y,color);
-        for (int i = 0; i < s.length() ; i++) {
-            menuAffichage[y] = setCharAt(menuAffichage[y],x+i,s.charAt(i));
-        }
+    public void printStringOnLine(int x, int y ,String s){
+        int xInitial = x;
+        for (int i = 0; i < s.length() && i+xInitial < LARGEUR_MENU-1; i++) {
+            int currenti = i;
+            String res = s.charAt(currenti)+"";
+            Pattern p = Pattern.compile(".\\[([0-9]{1,3})m");
 
+            // création d'un moteur de recherche
+            if( i < s.length() - 5){
+
+                Matcher m = p.matcher(s.substring(currenti, currenti+5));
+
+                if( m.find()){
+                    System.out.println("trouvé "+y +" "+i +" "+m.start() +" - "+m.end() );
+                        int size =m.end()-m.start();
+                        if( m.start() == 0){
+                            res = m.group(0);
+                            res += s.charAt(currenti+m.end());
+                            i = i+size;
+                        }
+                }
+            }
+            modifyCase(x,y,res);
+            ++x;
+        }
     }
 
     /**
@@ -148,11 +194,10 @@ public class Menu {
      * @param side 0 = left, 1 = right, 2 = center
      * @param strings le tableau de string
      * @param y Coordonnée y
-     * @param color  couleur des lignes du tableau
      */
-    public  void printStringOnSide(int side,String[] strings ,int y, String color){
+    public  void printStringOnSide(int side,String[] strings ,int y){
         for (int i = 0; i < strings.length; i++) {
-            printStringOnSide(side,strings[i],y+i,color);
+            printStringOnSide(side,strings[i],y+i);
         }
     }
 
@@ -161,51 +206,27 @@ public class Menu {
      * @param side  0 = left, 1 = right, 2 = center
      * @param s la string
      * @param y Coordonnée y
-     * @param color couleur de la ligne du string
      */
-    public void printStringOnSide(int side ,String s, int y , String color){
+    public void printStringOnSide(int side ,String s, int y ){
         switch (side){
             case 0 -> {//left
-                printStringOnLine(1,y,s,color);
+                printStringOnLine(1,y,s);
             }
             case 1 -> {//right
-                printStringOnLine(LARGEUR_MENU-s.length(),y,s,color);
+                printStringOnLine(LARGEUR_MENU-s.length(),y,s);
             }
             default -> {//center
                 int spacing = (LARGEUR_MENU-s.length())/2;
-                printStringOnLine(spacing,y,s,color);
+                printStringOnLine(spacing,y,s);
             }
         }
     }
 
-    /**
-     * Mémorise la couleur à affecter au tableau
-     * @param y Coordonnée y
-     * @param color la couleur
-     */
-    public void putColor(int y, String color){
-        colors[y] = color;
-    }
 
-    /**
-     * Ajoute la couleur au menu
-     * @param s la string
-     * @param y  Coordonnée y
-     */
-    public void setColorAt(String s,int y){
-        menuAffichage[y] = "║"+s+menuAffichage[y].substring(1,LARGEUR_MENU+1)+Affichage.BLUE+"║\n";
-    }
 
-    /**
-     * modifies un character à la string s au i
-     * @param s la string
-     * @param i l'index du char
-     * @param c le char
-     * @return la nouvelle string
-     */
-    public String setCharAt( String s , int i , char c){
-        return s.substring(0,i)+c+s.substring(i+1);
-    }
+ public void modifyCase(int x , int y , String s){
+        menuAffichage[y][x].container = s;
+ }
 
 
     /**
@@ -215,12 +236,31 @@ public class Menu {
      * @author Yann
      */
     public void toStringByLine(int line ,StringBuilder sb ) {
-
         if (line==0)refresh();
         sb.append(Affichage.BOLD).append(Affichage.BLUE).append("     ");
-        if (colors[line]!=null)setColorAt(colors[line],line);
-        sb.append(menuAffichage[line]);
+        sb.append(toStringLine(line));
     }
+
+
+    public String toStringLine(int line){
+        StringBuilder s = new StringBuilder();
+        for (int i = 0; i < menuAffichage[line].length; i++) {
+            s.append(menuAffichage[line][i]);
+        }
+        return s.toString();
+    }
+
+    public int numberOfColorChanges(String s){
+        Pattern p = Pattern.compile(".\\[([0-9]{1,3})m");
+        // création d'un moteur de recherche
+        Matcher m = p.matcher(s);
+        // lancement de la recherche de toutes les occurrences
+
+        long l = (m.results().count());
+
+        return  (int) l*2;
+    }
+
 
 
 }
